ac31c56fbdc3100962a20186389e28ad47d65977
Desire Banyeretse <banyerdes@gmail.com>
2025-09-22 18:43:51 -0500

    Added Flow Logs

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -173,3 +173,57 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "main" {
   tags = merge(var.standard_tags, var.project_tags, { Name = "${var.project_name}-${var.environment}-tgw-attachment" })
 }
 
+# --- VPC Flow Logs ---
+# The following resources enable VPC Flow Logs to be sent to CloudWatch for debugging.
+resource "aws_cloudwatch_log_group" "flow_logs" {
+  name = "/aws/vpc-flow-logs/${var.project_name}-${var.environment}"
+  tags = merge(var.standard_tags, var.project_tags, {
+    Name = "${var.project_name}-${var.environment}-flow-logs"
+  })
+}
+
+resource "aws_iam_role" "flow_logs_role" {
+  name = "${var.project_name}-${var.environment}-flow-logs-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "vpc-flow-logs.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "flow_logs_policy" {
+  name = "${var.project_name}-${var.environment}-flow-logs-policy"
+  role = aws_iam_role.flow_logs_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogGroups",
+          "logs:DescribeLogStreams"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_flow_log" "main" {
+  iam_role_arn    = aws_iam_role.flow_logs_role.arn
+  log_destination = aws_cloudwatch_log_group.flow_logs.arn
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.main.id
+
+  depends_on = [aws_iam_role_policy.flow_logs_policy]
+}
