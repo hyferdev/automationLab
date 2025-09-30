6fdab7cda8db625f5618aa151035082ec039967f
Desire Banyeretse <banyerdes@gmail.com>
2025-09-29 21:23:03 -0500

    Testing new providers

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,18 +1,19 @@
-#########################
-# --- Providers ---
 # /terraform/providers.tf
-#########################
+# Declares all Terraform providers used in this project.
 
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"
+    }
+    panos = {
+      source = "PaloAltoNetworks/panos"
+      version = "~> 2.0.0"
     }
   }
-
-    cloud {
-    }
+  cloud {
+  }
 }
 
 provider "aws" {
