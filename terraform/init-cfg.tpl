b3306f94f3fb7cfa1d2eb5754ceef616d1569934
Desire Banyeretse <banyerdes@gmail.com>
2025-12-28 13:58:15 -0600

    Removed PaloAlto Bootstrap

--- terraform/init-cfg.tpl
+++
@@ -1,9 +0,0 @@
-# /terraform/init-cfg.tpl
-# This template is used to generate the init-cfg.txt file.
-
-# Basic operational settings
-type=dhcp-client
-op-command-modes=jumbo-frame
-op-command-modes=mgmt-interface-swap
-plugin-op-commands=aws-gwlb-inspect:enable
-plugin-op-commands=aws-gwlb-overlay-routing:enable
--- terraform/paloalto.tf
+++ terraform/paloalto.tf
@@ -1,37 +1,6 @@
 # /terraform/paloalto.tf
 # Deploys the Palo Alto Networks VM-Series firewalls and their resources.
 
-# --- IAM Role for Bootstrapping ---
-resource "aws_iam_role" "paloalto_bootstrap_role" {
-  name = "${var.project_name}-${var.environment}-paloalto-bootstrap-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = { Service = "ec2.amazonaws.com" }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy" "paloalto_bootstrap_policy" {
-  name = "${var.project_name}-${var.environment}-paloalto-bootstrap-policy"
-  role = aws_iam_role.paloalto_bootstrap_role.id
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action   = ["s3:GetObject"],
-      Effect   = "Allow",
-      Resource = "${aws_s3_bucket.bootstrap_bucket.arn}/*"
-    }]
-  })
-}
-
-resource "aws_iam_instance_profile" "paloalto_bootstrap_profile" {
-  name = "${var.project_name}-${var.environment}-paloalto-bootstrap-profile"
-  role = aws_iam_role.paloalto_bootstrap_role.name
-}
-
 # --- Security Group for Management Access ---
 resource "aws_security_group" "paloalto_mgmt_sg" {
   name        = "${var.project_name}-${var.environment}-paloalto-mgmt-sg"
@@ -143,8 +112,10 @@ resource "aws_eip_association" "paloalto_mgmt_eip_assoc" {
 # Interface 1: Data (Public Egress) - Maps to ethernet1/1
 resource "aws_network_interface" "paloalto_data" {
   for_each = toset(var.availability_zones)
+
   subnet_id         = module.vpc["security"].egress_subnet_ids_by_az[each.key]
-  security_groups   = [aws_security_group.paloalto_data_sg.id]  
+  security_groups   = [aws_security_group.paloalto_data_sg.id]
+
   source_dest_check = false
   tags              = { Name = "${var.project_name}-${var.environment}-pa-data-${each.key}" }
 }
@@ -163,34 +134,21 @@ resource "aws_instance" "paloalto" {
   ami                  = data.aws_ami.paloalto.id
   instance_type        = var.paloalto_instance_type
   availability_zone    = each.key
-  iam_instance_profile = aws_iam_instance_profile.paloalto_bootstrap_profile.name
   key_name             = var.ssh_key_name
 
-  # Attach the primary (management) interface
-  primary_network_interface {
+  # Interface 0: Management
+  network_interface {
     network_interface_id = aws_network_interface.paloalto_mgmt[each.key].id
+    device_index         = 0
   }
 
-  # Use user_data to set the initial admin password
-  user_data = "vmseries-bootstrap-aws-s3-bucket=${aws_s3_bucket.bootstrap_bucket.id}"
-  user_data_replace_on_change = true
-
-  # Ensure the firewall is recreated if the Data interface is replaced.
-  lifecycle {
-    replace_triggered_by = [
-      aws_network_interface.paloalto_data[each.key]
-    ]
+  # Interface 1: Data
+  network_interface {
+    network_interface_id = aws_network_interface.paloalto_data[each.key].id
+    device_index         = 1
   }
 
   tags = merge(var.standard_tags, var.project_tags, {
     Name = "${var.project_name}-${var.environment}-paloalto-${each.key}"
   })
-}
-
-# Attach Data Interface (eth1/1)
-resource "aws_network_interface_attachment" "paloalto_attach_data" {
-  for_each             = toset(var.availability_zones)
-  instance_id          = aws_instance.paloalto[each.key].id
-  network_interface_id = aws_network_interface.paloalto_data[each.key].id
-  device_index         = 1
 }
